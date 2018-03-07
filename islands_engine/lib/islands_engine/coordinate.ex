941d94fae838066e134f56b8427003368b786355
dspain <dmspain@yahoo.com>
2018-03-06 21:54:48 -0800

    create coordinate using row & col

---
+++ islands_engine/lib/islands_engine/coordinate.ex
@@ -0,0 +1,8 @@
+defmodule IslandsEngine.Coordinate do
+  alias __MODULE__
+
+  @enforce_keys [:row, :col]
+  defstruct [:row, :col]
+
+  def new(row, col), do: {:ok, %Coordinate{row: row, col: col}}
+end
