4a64cecd9a5b6a37082c08d6010e47534b048c4e
dspain <dmspain@yahoo.com>
2018-03-08 21:17:15 -0800

    function to add coordinates to the island, erroring out if coordinates are not on the board

--- islands_engine/lib/islands_engine/island.ex
+++ islands_engine/lib/islands_engine/island.ex
@@ -18,4 +18,17 @@ defmodule IslandsEngine.Island do
   defp offsets(:l_shape), do: [{0, 0}, {1, 0}, {2, 0}, {2, 1}]
   defp offsets(:s_shape), do: [{0, 1}, {0, 2}, {1, 0}, {1, 1}]
   defp offsets(_), do: {:error, :invalid_island_type}
+
+  defp add_coordinates(offsets, upper_left) do
+    Enum.reduce_while(offsets, MapSet.new(), fn offset, acc ->
+      add_coordinate(acc, upper_left, offset)
+    end)
+  end
+
+  defp add_coordinate(coordinates, %Coordinate{row: row, col: col}, {row_offset, col_offset}) do
+    case Coordinate.new(row + row_offset, col + col_offset) do
+      {:ok, coordinate} -> {:cont, MapSet.put(coordinates, coordinate)}
+      {:error, :invalid_coordinate} -> {:halt, {:error, :invalid_coordinate}}
+    end
+  end
 end
