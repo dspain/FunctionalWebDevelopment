d17fb700503cef620318f8464d58e8213ae1c30e
dspain <dmspain@yahoo.com>
2018-03-08 21:09:20 -0800

    add offsets for all the different island types

--- islands_engine/lib/islands_engine/island.ex
+++ islands_engine/lib/islands_engine/island.ex
@@ -11,4 +11,11 @@ defmodule IslandsEngine.Island do
   Create a new island
   """
   def new(), do: %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
+
+  defp offsets(:square), do: [{0, 0}, {0, 1}, {1, 0}, {1, 1}]
+  defp offsets(:atoll), do: [{0, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}]
+  defp offsets(:dot), do: [{0, 0}]
+  defp offsets(:l_shape), do: [{0, 0}, {1, 0}, {2, 0}, {2, 1}]
+  defp offsets(:s_shape), do: [{0, 1}, {0, 2}, {1, 0}, {1, 1}]
+  defp offsets(_), do: {:error, :invalid_island_type}
 end
