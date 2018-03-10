8c94417d965a57b1ffcc66a3203f3729bff98cab
dspain <dmspain@yahoo.com>
2018-03-09 21:27:20 -0800

    add guess tests

--- islands_engine/lib/islands_engine/guesses.ex
+++ islands_engine/lib/islands_engine/guesses.ex
@@ -52,6 +52,39 @@ defmodule IslandsEngine.Guesses do
 
   @doc """
   Add a coordinate to list of hits and misses
+
+  ## Examples
+
+    iex> guesses = Guesses.new()
+    %Guesses{hits: Enum.into([], MapSet.new()), misses: Enum.into([], MapSet.new())}
+    iex> {:ok, coordinate1} = Coordinate.new(8,3)
+    {:ok, %Coordinate{row: 8, col: 3}}
+    iex> guesses = Guesses.add(guesses, :hit, coordinate1)
+    %Guesses{
+      hits: [%Coordinate{row: 8, col: 3}] |>
+            Enum.into(MapSet.new()),
+      misses: Enum.into([], MapSet.new())
+    }
+    iex> {:ok, coordinate2} = Coordinate.new(9,7)
+    {:ok, %Coordinate{row: 9, col: 7}}
+    iex> guesses = Guesses.add(guesses, :hit, coordinate2)
+    %Guesses{
+      hits: [
+        %Coordinate{row: 8, col: 3},
+        %Coordinate{row: 9, col: 7}
+      ] |> Enum.into(MapSet.new()),
+      misses: Enum.into([], MapSet.new())
+    }
+    iex> {:ok, coordinate3} = Coordinate.new(1,2)
+    {:ok, %Coordinate{row: 1, col: 2}}
+    iex> Guesses.add(guesses, :miss, coordinate3)
+    %Guesses{
+      hits: [
+        %Coordinate{row: 8, col: 3},
+        %Coordinate{row: 9, col: 7}
+      ] |> Enum.into(MapSet.new()),
+      misses: [%Coordinate{row: 1, col: 2}] |> Enum.into(MapSet.new())
+    }
   """
   def add(%Guesses{} = guesses, :hit, %Coordinate{} = coordinate) do
     update_in(guesses.hits, &MapSet.put(&1, coordinate))
