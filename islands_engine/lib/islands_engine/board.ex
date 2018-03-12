5478ea7e941915fa121d443066623d3f2ef17299
dspain <dmspain@yahoo.com>
2018-03-11 21:38:49 -0700

    add board tests

--- islands_engine/lib/islands_engine/board.ex
+++ islands_engine/lib/islands_engine/board.ex
@@ -7,9 +7,37 @@ defmodule IslandsEngine.Board do
 
   @doc """
   Create a new board
+
+  ## Examples
+      iex> Board.new()
+      %{}
   """
   def new(), do: %{}
 
+  @doc """
+  Position an island on the board
+
+  ## Examples
+      iex> board = Board.new()
+      iex> {:ok, square_coordinate} = Coordinate.new(1,1)
+      iex> {:ok, square} = Island.new(:square, square_coordinate)
+      iex> board = Board.position_island(board, :square, square)
+      %{
+        square: %Island{
+          coordinates: [
+            %Coordinate{col: 1, row: 1},
+            %Coordinate{col: 1, row: 2},
+            %Coordinate{col: 2, row: 1},
+            %Coordinate{col: 2, row: 2}
+          ] |> Enum.into(MapSet.new()),
+          hit_coordinates: Enum.into([], MapSet.new())
+        }
+      }
+      iex> {:ok, dot_coordinate} = Coordinate.new(2,2)
+      iex> {:ok, dot} = Island.new(:dot, dot_coordinate)
+      iex> Board.position_island(board, :dot, dot)
+      {:error, :overlapping_island}
+  """
   def position_island(board, key, %Island{} = island) do
     case overlaps_existing_island?(board, key, island) do
       true -> {:error, :overlapping_island}
@@ -27,6 +55,93 @@ defmodule IslandsEngine.Board do
     Enum.all?(Island.types(), &Map.has_key?(board, &1))
   end
 
+  @doc """
+  Guess if a coordinate is a hit or miss,  return a 4-tuple of information:
+    {:hit/:miss, :none/:island_type, :win/:no_win, board}
+
+  ## Examples
+      iex> board = Board.new()
+      iex> {:ok, square_coordinate} = Coordinate.new(1,1)
+      iex> {:ok, square} = Island.new(:square, square_coordinate)
+      iex> board = Board.position_island(board, :square, square)
+      iex> {:ok, dot_coordinate} = Coordinate.new(3,3)
+      iex> {:ok, dot} = Island.new(:dot, dot_coordinate)
+      iex> board = Board.position_island(board, :dot, dot)
+      iex> {:ok, guess_coordinate} = Coordinate.new(10,10)
+      iex> {:miss, :none, :no_win, board} = Board.guess(board, guess_coordinate)
+      {:miss, :none, :no_win,
+        %{
+          square: %Island{
+            coordinates: [
+              %Coordinate{col: 1, row: 1},
+              %Coordinate{col: 1, row: 2},
+              %Coordinate{col: 2, row: 1},
+              %Coordinate{col: 2, row: 2}
+            ] |> Enum.into(MapSet.new()),
+            hit_coordinates: Enum.into([], MapSet.new())
+          },
+          dot: %Island{
+            coordinates: [
+              %Coordinate{col: 3, row: 3}
+            ] |> Enum.into(MapSet.new()),
+            hit_coordinates: Enum.into([], MapSet.new())
+          }
+        }
+      }
+      iex> {:ok, hit_coordinate} = Coordinate.new(1,1)
+      iex> {:hit, :none, :no_win, board} = Board.guess(board, hit_coordinate)
+      {:hit, :none, :no_win,
+        %{
+          square: %Island{
+            coordinates: [
+              %Coordinate{col: 1, row: 1},
+              %Coordinate{col: 1, row: 2},
+              %Coordinate{col: 2, row: 1},
+              %Coordinate{col: 2, row: 2}
+            ] |> Enum.into(MapSet.new()),
+            hit_coordinates: [
+              %Coordinate{col: 1, row: 1}
+            ] |> Enum.into(MapSet.new())
+          },
+          dot: %Island{
+            coordinates: [
+              %Coordinate{col: 3, row: 3}
+            ] |> Enum.into(MapSet.new()),
+            hit_coordinates: Enum.into([], MapSet.new())
+          }
+        }
+      }
+      iex> square = %{square | hit_coordinates: square.coordinates}
+      iex> board = Board.position_island(board, :square, square)
+      iex> {:ok, win_coordinate} = Coordinate.new(3,3)
+      iex> {:hit, :dot, :win, _board} = Board.guess(board, win_coordinate)
+      {:hit, :dot, :win,
+        %{
+          square: %Island{
+            coordinates: [
+              %Coordinate{col: 1, row: 1},
+              %Coordinate{col: 1, row: 2},
+              %Coordinate{col: 2, row: 1},
+              %Coordinate{col: 2, row: 2}
+            ] |> Enum.into(MapSet.new()),
+            hit_coordinates: [
+              %Coordinate{col: 1, row: 1},
+              %Coordinate{col: 1, row: 2},
+              %Coordinate{col: 2, row: 1},
+              %Coordinate{col: 2, row: 2}
+            ] |> Enum.into(MapSet.new())
+          },
+          dot: %Island{
+            coordinates: [
+              %Coordinate{col: 3, row: 3}
+            ] |> Enum.into(MapSet.new()),
+            hit_coordinates: [
+              %Coordinate{col: 3, row: 3}
+            ] |> Enum.into(MapSet.new()),
+          }
+        }
+      }
+  """
   def guess(board, %Coordinate{} = coordinate) do
     board
     |> check_all_islands(coordinate)
--- islands_engine/test/doc_test.exs
+++ islands_engine/test/doc_test.exs
@@ -1,7 +1,8 @@
 defmodule DocTest do
   use ExUnit.Case
-  alias IslandsEngine.{Coordinate, Guesses, Island}
+  alias IslandsEngine.{Coordinate, Guesses, Island, Board}
   doctest Coordinate
   doctest Guesses
   doctest Island
+  doctest Board
 end
