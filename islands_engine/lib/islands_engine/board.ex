992ea94f16b5384d6736af4c13d179a849fca872
dspain <dmspain@yahoo.com>
2018-03-11 20:45:04 -0700

    check if new island overlaps an existing island

--- islands_engine/lib/islands_engine/board.ex
+++ islands_engine/lib/islands_engine/board.ex
@@ -16,4 +16,10 @@ defmodule IslandsEngine.Board do
       false -> Map.put(board, key, island)
     end
   end
+
+  def overlaps_existing_island?(board, new_key, new_island) do
+    Enum.any?(board, fn {key, island} ->
+      key != new_key and Island.overlaps?(island, new_island)
+    end)
+  end
 end
