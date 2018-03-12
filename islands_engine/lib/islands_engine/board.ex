224bd6b929f0fe504da3782a7feb2c43ad34ec62
dspain <dmspain@yahoo.com>
2018-03-11 20:42:01 -0700

    position island by putting it on the board as long as it doesn't overlap an existing island

--- islands_engine/lib/islands_engine/board.ex
+++ islands_engine/lib/islands_engine/board.ex
@@ -1,3 +1,19 @@
 defmodule IslandsEngine.Board do
+  @moduledoc """
+  Description of `IslandsEngine.Board`
+  """
+
+  alias IslandsEngine.Island
+
+  @doc """
+  Create a new board
+  """
   def new(), do: %{}
+
+  def position_island(board, key, %Island{} = island) do
+    case overlaps_existing_island?(board, key, island) do
+      true -> {:error, :overlapping_island}
+      false -> Map.put(board, key, island)
+    end
+  end
 end
