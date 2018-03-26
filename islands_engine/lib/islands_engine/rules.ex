8923199b28733ba2d8c2e33f777bb2472a4aeb17
dspain <dmspain@yahoo.com>
2018-03-25 20:16:25 -0700

    add check for players to position their islands

--- islands_engine/lib/islands_engine/rules.ex
+++ islands_engine/lib/islands_engine/rules.ex
@@ -1,7 +1,9 @@
 defmodule IslandsEngine.Rules do
   alias __MODULE__
 
-  defstruct state: :initialized
+  defstruct state: :initialized,
+            player1: :islands_not_set,
+            player2: :islands_not_set
 
   @doc """
   Create a new 'Rules' struct with state `:initialized`
@@ -27,6 +29,13 @@ defmodule IslandsEngine.Rules do
   """
   def check(%Rules{state: :initialized} = rules, :add_player),
     do: {:ok, %Rules{rules | state: :player_set}}
+
+  def check(%Rules{state: :player_set} = rules, {:position_islands, player}) do
+    case Map.fetch(rules, player) do
+      :islands_set -> :error
+      :islands_not_set -> {:ok, rules}
+    end
+  end
 
   def check(_state, _action), do: :error
 end
